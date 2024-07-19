47f4aef0cd9a30a09a19a2d4bc0c5f430074d46c
Edwin Portillo <edwin.portillo@calltrackingmetrics.com>
2024-07-19 09:04:58 -0400

    wipperoni

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,7 +1,9 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = {
+  "nvim-treesitter/nvim-treesitter",
+  event = "BufRead", -- Lazy load on buffer read
+  run = ":TSUpdate",
+  config = function()
+    require("nvim-treesitter.configs").setup({
       ensure_installed = {
         "bash",
         "html",
@@ -19,6 +21,16 @@ return {
         "yaml",
         "ruby",
       },
-    },
-  },
+      highlight = {
+        enable = true,
+        disable = function(lang, buf)
+          local max_filesize = 100 * 1024 -- 100 KB
+          local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+          if ok and stats and stats.size > max_filesize then
+            return true
+          end
+        end,
+      },
+    })
+  end,
 }
